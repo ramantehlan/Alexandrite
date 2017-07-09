2e9f55c68f1b63aafaf3ad2bbb822962e9777cb9
Raman Tehlan <ramantehlan@gmail.com>
2017-07-09 17:28:23 +0530

    First Commit

--- app/src/main/java/com/diamond/diamond/Files.java
+++ app/src/main/java/com/diamond/diamond/Files.java
@@ -3,6 +3,7 @@ package com.diamond.diamond;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,26 +15,38 @@ import java.io.IOException;
 
 public class Files extends AppCompatActivity {
 
+    /**
+     * To check if external storage is mounted or not
+     */
+    // For writing external storage
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files);
 
 
-           //Toast.makeText(getBaseContext() , this.getFilesDir().toString() , Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getBaseContext() , this.getFilesDir().toString() , Toast.LENGTH_SHORT).show();
 
 
-      // Comman variables
+        // Comman variables
         String filename = "newfile.txt";
         FileOutputStream writeToFile;
         FileInputStream readFromFile;
         TextView display = (TextView) findViewById(R.id.fileDisplay);
 
 
-
         // This is to store data and read data from the internal storage
-        try{
-            writeToFile = openFileOutput(filename , Context.MODE_PRIVATE);
+        try {
+            writeToFile = openFileOutput(filename, Context.MODE_PRIVATE);
             writeToFile.write("sf as1231232!@#@!#@#$#$%$".getBytes());
             writeToFile.close();
 
@@ -42,32 +55,32 @@ public class Files extends AppCompatActivity {
 
             int c;
             String temp = "";
-            while ((c = readFromFile.read()) != -1){
-                temp = temp + Character.toString((char)c);
+            while ((c = readFromFile.read()) != -1) {
+                temp = temp + Character.toString((char) c);
             }
             readFromFile.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         // This is to create the temp file
         File file;
-        try{
+        try {
             String tempFilename = Uri.parse("http://www.google.com/file/test/back/new.html").getLastPathSegment();
-            file = File.createTempFile(tempFilename , null , this.getCacheDir());
+            file = File.createTempFile(tempFilename, null, this.getCacheDir());
             // Above will create new.html
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
 
         // This is to create new directory
-        String path = this.getFilesDir().toString() +  "/userfiles/check.txt";
+        String path = this.getFilesDir().toString() + "/userfiles/check.txt";
         File f = new File(path);
-        if(f.mkdirs()){
-           // Toast.makeText(getBaseContext() , "created " + path , Toast.LENGTH_LONG).show();
-        }else{
+        if (f.mkdirs()) {
+            // Toast.makeText(getBaseContext() , "created " + path , Toast.LENGTH_LONG).show();
+        } else {
             // Toast.makeText(getBaseContext() , "failed" , Toast.LENGTH_LONG).show();
             f.delete();
         }
@@ -78,13 +91,13 @@ public class Files extends AppCompatActivity {
         File l = new File(full);
         String[] fileDir = l.list();
         String list = "";
-        for(int a = 0; a < fileDir.length ; a++){
+        for (int a = 0; a < fileDir.length; a++) {
             list = list + "\n" + fileDir[a];
         }
 
         display.setText(list);
 
-        Toast.makeText(getBaseContext() , full , Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), full, Toast.LENGTH_LONG).show();
 
         // This is another test comment
 
