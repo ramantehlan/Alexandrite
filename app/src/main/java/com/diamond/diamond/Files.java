824143c29a9ae4072ea15515758b6ea0d0470a59
Raman Tehlan <ramantehlan@gmail.com>
2017-07-14 00:31:23 +0530

    Improved comment setup

--- app/src/main/java/com/diamond/diamond/Files.java
+++ app/src/main/java/com/diamond/diamond/Files.java
@@ -20,6 +20,9 @@ import static com.diamond.diamond.R.id.sdPublicDirectoryTree;
 
 public class Files extends AppCompatActivity {
 
+    /**
+     * Global variables are declared here
+     */
     // Tag to log
     private static final String FILES_TAG = "tag.files";
     // Array of TextView to display the directory list
@@ -31,6 +34,10 @@ public class Files extends AppCompatActivity {
     public String[] properties;
     // Array of Files to store rootFiles
     public File[] rootFiles;
+
+    /**
+     * Main program start from here
+     */
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,7 +56,7 @@ public class Files extends AppCompatActivity {
 
         properties = new String[]{"internal", "external", "external"};
 
-        rootFiles = new File[]{ this.getCacheDir(),
+        rootFiles = new File[]{this.getCacheDir(),
                 this.getExternalFilesDir(null).getParentFile(),
                 Environment.getExternalStorageDirectory()
         };
@@ -66,47 +73,19 @@ public class Files extends AppCompatActivity {
             }
         }
 
-        // These are cache details
-        File internalCacheDir = this.getCacheDir();
-        File externalCacheDir = this.getExternalCacheDir();
-        String internalCacheName = "internalCacheText2";
-        String externalCacheName = "externalCacheText";
-
-
         // This is to test if cache some file to internal cache
-        if(this.createCacheFile(internalCacheDir , internalCacheName )){
-            try {
-                File file = new File(this.getCacheDir(), internalCacheName);
-                FileWriter cache = new FileWriter(file.getAbsoluteFile());
-                BufferedWriter bw = new BufferedWriter(cache);
-                bw.write("this is the internal cache content \n this is line 2 \n this is line 3");
-                bw.close();
-
-                Log.i(FILES_TAG , "(internal) Just wrote to " + internalCacheDir.toString() + internalCacheName);
-
-                Log.i(FILES_TAG , "(internal) Just read  from " + internalCacheDir.toString() + internalCacheName);
-                FileReader cacheReader = new FileReader(file.getAbsoluteFile());
-                BufferedReader br = new BufferedReader(cacheReader);
-
-                String cacheData;
-                String finalCacheData = "";
-                while ((cacheData = br.readLine()) != null){
-                        finalCacheData += cacheData + "\n";
-                }
-
-                Log.i(FILES_TAG , finalCacheData);
-
-
-                br.close();
-
-
-            }catch (Exception e){
-                e.printStackTrace();
-                Log.e(FILES_TAG , "Failed to write to internal cache file.");
-            }
-        }else{
-            Log.e(FILES_TAG , "Failed to create internal cache file.");
+        if (this.createCacheFile(this.getCacheDir(), "inCacheFile", "this is cache content of internal cache")) {
+            Log.e(FILES_TAG, "Internal cache file created successfully.");
+        } else {
+            Log.e(FILES_TAG, "Failed to create internal cache file.");
         }
+
+        if(this.createCacheFile(this.getExternalCacheDir() , "exCacheFile" , "this is external cache content")){
+            Log.e(FILES_TAG, "External cache file created successfully");
+        } else {
+            Log.e(FILES_TAG, "Failed to create external cache file.");
+        }
+
 
         // This is to update the external board
         TextView exRead = (TextView) findViewById(R.id.exRead);
@@ -114,39 +93,43 @@ public class Files extends AppCompatActivity {
         TextView exEmulated = (TextView) findViewById(R.id.exEmulated);
 
         String exReadable = exRead.getText().toString();
-        exReadable += (isExternalStorageReadable()) ?  " Readable" : " Not Readable";
+        exReadable += (isExternalStorageReadable()) ? " Readable" : " Not Readable";
         exRead.setText(exReadable);
 
         String exWritable = exWrite.getText().toString();
-        exWritable += (isExternalStorageWritable()) ?  " Mounted" : " Not Mounted";
+        exWritable += (isExternalStorageWritable()) ? " Mounted" : " Not Mounted";
         exWrite.setText(exWritable);
 
         String emulated = exEmulated.getText().toString();
-        emulated += (Environment.isExternalStorageEmulated())?  " Emulated" : " Not Emulated";
+        emulated += (Environment.isExternalStorageEmulated()) ? " Emulated" : " Not Emulated";
         exEmulated.setText(emulated);
     }
 
 
-    // This function is called to display the directories
+    /**
+     * To get list of directories
+     * @param path path of which directory list is requested
+     * @return string which contain list of directory list
+     */
     public String getDirectoryTree(String path) {
         File file = new File(path);
         String[] fileDirs = file.list();
         String list = "";
 
-        if(file.exists() && fileDirs != null){
-        // This is to create the list of directories
-        for (String tempDirName : fileDirs) {
+        if (file.exists() && fileDirs != null) {
+            // This is to create the list of directories
+            for (String tempDirName : fileDirs) {
 
-            File tempDir = new File(tempDirName);
-            list += "- " + tempDirName;
-            list += (tempDir.isDirectory()) ? "[Dir]" : "";
-            list += (tempDir.isFile()) ? "[File]" : "";
-            list += (tempDir.canRead()) ? "[R]" : "[NR]";
-            //list += (tempDir.isHidden()) ? "[Hidden]" : "[Not Hidden]";
-            //list += (tempDir.exists()) ? "[Existing]" : "[Non Existing]";
-            list += "\n";
+                File tempDir = new File(tempDirName);
+                list += "- " + tempDirName;
+                list += (tempDir.isDirectory()) ? "[Dir]" : "";
+                list += (tempDir.isFile()) ? "[File]" : "";
+                list += (tempDir.canRead()) ? "[R]" : "[NR]";
+                //list += (tempDir.isHidden()) ? "[Hidden]" : "[Not Hidden]";
+                //list += (tempDir.exists()) ? "[Existing]" : "[Non Existing]";
+                list += "\n";
 
-        }
+            }
         }
 
         // finally return the list of directories
@@ -158,17 +141,31 @@ public class Files extends AppCompatActivity {
     }
 
 
-    // Check if external storage is available for read and write
+    /**
+     * To check if external storage is mounted or not
+     * @return boolean
+     */
     public boolean isExternalStorageWritable() {
         return (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()));
     }
 
-    // Check if external storage is available for at least read
+    /**
+     * To if external storage is mounted and readable
+     * @return boolean
+     */
     public boolean isExternalStorageReadable() {
         return (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(Environment.getExternalStorageState()));
     }
 
-    public void displayDirectoryTree(int position) {
+    /**
+     * To display directory according to the position
+     * @param position this is the number which specify the index in following arrays
+     *                 directoriesView
+     *                 propertiesView
+     *                 properties
+     *                 rootFiles
+     */
+    private void displayDirectoryTree(int position) {
         // To display the directories of rootFile by calling displayDirectoryTree function
         directoriesView[position].setText(this.getDirectoryTree(rootFiles[position].toString()));
 
@@ -182,22 +179,64 @@ public class Files extends AppCompatActivity {
         propertiesView[position].setText(propertiesView[position].getText().toString() + rootFiles[position].toString() + properties[position]);
     }
 
-    // To create cache file and return boolean
-    public boolean createCacheFile(File path , String cacheFileName){
+    /**
+     * To create a cache file
+     * @param cacheDirectoryPath path of cache directory
+     * @param cacheFileName name of cache file
+     * @param cacheContent content of cache file
+     * @return boolean
+     */
+    public boolean createCacheFile(File cacheDirectoryPath, String cacheFileName, String cacheContent) {
         try {
-            if(path.exists()) {
-                path = File.createTempFile(cacheFileName, null, path);
-                Log.i(FILES_TAG , "Just created " + path.toString() + cacheFileName);
+            if (cacheDirectoryPath.exists()) {
+                cacheDirectoryPath = File.createTempFile(cacheFileName, null, cacheDirectoryPath);
+                File cacheFile = new File(cacheDirectoryPath, cacheFileName);
+
+                FileWriter cacheFileWriter = new FileWriter(cacheFile.getAbsoluteFile());
+                BufferedWriter cacheFileBuffWriter = new BufferedWriter(cacheFileWriter);
+                cacheFileBuffWriter.write(cacheContent);
+                cacheFileBuffWriter.close();
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             // To print the error
             // We can also use file to store the error
             e.printStackTrace();
             return false;
         }
+    }
+
+    /**
+     * To read a cache file
+     * @param cacheDirectoryPath path of cache directory
+     * @param cacheFileName name of cache file
+     * @return content of cache file
+     */
+    public String readCacheFile(File cacheDirectoryPath, String cacheFileName) {
+        String finalCacheData = "";
+        try {
+            cacheDirectoryPath = new File(cacheDirectoryPath, cacheFileName);
+            // Check if cache file we are trying to read exists or not
+            if(cacheDirectoryPath.exists()) {
+                FileReader cacheReader = new FileReader(cacheDirectoryPath.getAbsoluteFile());
+                BufferedReader br = new BufferedReader(cacheReader);
+
+                String cacheDataLine;
+                while ((cacheDataLine = br.readLine()) != null) {
+                    finalCacheData += cacheDataLine + "\n";
+                }
+
+                br.close();
+            }else{
+                finalCacheData = null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return finalCacheData;
     }
 
 }
