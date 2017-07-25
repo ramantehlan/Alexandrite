d86e614cf7c71861f1658c20bffdc02d818e802d
Raman Tehlan <ramantehlan@gmail.com>
2017-07-25 09:22:53 +0530

    Added Contract

--- app/src/main/java/com/alexandrite/first/ContractFeed.java
+++ app/src/main/java/com/alexandrite/first/ContractFeed.java
@@ -5,14 +5,14 @@ import android.provider.BaseColumns;
 public class ContractFeed {
 
     // To create table feed
-    private static final String SQL_CREATE_ENTRIES =
+    public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE IF NOT EXISTS " + FeedEntry.TABLE_NAME + "(" +
                     FeedEntry._ID + " INT PRIMARY KEY ," +
                     FeedEntry.COLUMN_NAME_TITLE + " TEXT ," +
                     FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
 
     // To delete table feed
-    private static final String SQL_DELETE_ENTRIES =
+    public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
 
     // To Prevent someone from accidentally instantiating the contract class,
@@ -26,4 +26,7 @@ public class ContractFeed {
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_SUBTITLE = "subtitle";
     }
+
+
 }
+
--- app/src/main/java/com/alexandrite/first/FeedReaderDbHelper.java
+++ app/src/main/java/com/alexandrite/first/FeedReaderDbHelper.java
@@ -1,8 +1,28 @@
 package com.alexandrite.first;
 
-/**
- * Created by Raman on 23-07-2017.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DbHelperFeed {
+public class FeedReaderDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema then you must increment the database version
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    public FeedReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ContractFeed.SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache of online data, so its upgrade policy is
+        // to simply to display the data and start over
+        db.execSQL(ContractFeed.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
 }
